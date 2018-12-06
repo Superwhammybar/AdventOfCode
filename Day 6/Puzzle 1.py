4c67b370091f057901c16dd2f33842866fce331c
Oliver Whitwam <Oliverwhitwam@yahoo.com>
2018-12-06 08:07:55 +0000

    Day 6 Inputs and Day 6 Puzzle 1 initial idea

---
+++ Day 6/Puzzle 1.py
@@ -0,0 +1,10 @@
+# Split coords by ',
+# Assign name to each (letter?) pair
+# For each letter, plot on a list of lists board and
+# Mark each 'coord' in list of lists with the letter and it's distance to the letter
+# Use difference between row numbers and column numbers added together for distance
+# On each new letter, if the distance to the letter is shorter than the current owner of the coord then replace
+# If they're equal, mark with .?
+# Iterate over final board and count the number of each letter present
+# Most letters = winner.
+
